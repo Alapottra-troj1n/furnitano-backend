386c711e4d1a06951b6674b1e47058cae6843802
Alapottra <alapottratrojon@gmail.com>
2022-04-30 12:16:18 +0600

    added fetch by id api

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 
 //connect to mongodb
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://furnitano-admin:${process.env.DB_PASS}@furnitano.ykxho.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -27,7 +27,7 @@ const run = async() =>{
         const database = client.db('furnitano');
         const productCollection = database.collection('products');
 
-        //home api 
+        
        
 
         app.get('/products', async(req, res) =>{
@@ -36,6 +36,16 @@ const run = async() =>{
             const cursor = await productCollection.find(query);
             const products = await cursor.toArray();
             res.send(products);
+
+        })
+
+        app.get('/products/:id', async(req, res) =>{
+            const id = req.params.id;
+            const query = { _id: ObjectId(id)};
+            const user = await productCollection.findOne(query);
+            res.send(user);
+
+            
 
         })
 
@@ -51,7 +61,7 @@ const run = async() =>{
 
 run().catch(console.dir);
 
-
+//home api 
 app.get('/', async(req, res) =>{
     res.json({message : 'hello world'})
 
