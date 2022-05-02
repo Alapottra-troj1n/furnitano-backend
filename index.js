299bbc90edf5f5573251d6b909cf28f4dbd6d5e2
Alapottra <alapottratrojon@gmail.com>
2022-05-03 00:41:17 +0600

    added email to product

--- index.js
+++ index.js
@@ -90,6 +90,7 @@ const run = async() =>{
             const addedProduct = req.body;
             const newProduct = {
                 "image" : addedProduct.productImageUrl,
+                "email" : addedProduct.email,
                 "name" : addedProduct.productName,
                 "description" : addedProduct.productDescription,
                 "price" : addedProduct.productPrice,
@@ -103,20 +104,7 @@ const run = async() =>{
         })
 
 
-        // "image": "https://i.ibb.co/GPdTz46/5.webp",
-        // "name": "Morr Leather Chair",
-        // "description": "You and your family have a new go-to comfort spot when you add this double manual reclining sofa to your den or living room. It's upholstered in a mix of rich brown top grain leather and matching tone faux leather to match most decor.",
-        // "price": 350,
-        // "supplier": "Morr",
-        // "quantity": 15
-
-        // const productName = e.target.addProductName.value;
-        // const productEmail = email;
-        // const productPrice = e.target.addPrice.value;
-        // const productImageUrl = e.target.addImage.value;
-        // const productDescription = e.target.addDes.value;
-        // const productSupplier = e.target.addSupplier.value;
-        // const productQuantity = e.target.addQuantity.value;
+    
 
  
 
