4e0af871269320ad218b97e7dc0613d3e8a26aff
Alapottra <alapottratrojon@gmail.com>
2022-05-03 17:06:32 +0600

    added myinventory api

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const slugify = require('slugify');
-require('dotenv').config(); 
+require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 
 
 //middlewares
-app.use(cors()); 
-app.use(express.json()); 
+app.use(cors());
+app.use(express.json());
 
 
 
@@ -19,18 +19,18 @@ const uri = `mongodb+srv://furnitano-admin:${process.env.DB_PASS}@furnitano.ykxh
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
-const run = async() =>{
+const run = async () => {
 
 
-    try{
+    try {
         await client.connect();
         const database = client.db('furnitano');
         const productCollection = database.collection('products');
 
-        
-       //Show all products functionality 
 
-        app.get('/products', async(req, res) =>{
+        //Show all products functionality 
+
+        app.get('/products', async (req, res) => {
 
             const query = {};
             const cursor = await productCollection.find(query);
@@ -41,22 +41,22 @@ const run = async() =>{
 
         //Product by id functionality
 
-        app.get('/products/:id', async(req, res) =>{
+        app.get('/products/:id', async (req, res) => {
             const id = req.params.id;
-            const query = { _id: ObjectId(id)};
+            const query = { _id: ObjectId(id) };
             const user = await productCollection.findOne(query);
             res.send(user);
 
-            
+
 
         })
 
         //Delivered Functionality
 
-        app.put('/deliver/:id', async(req, res) =>{
+        app.put('/deliver/:id', async (req, res) => {
             const id = req.params.id;
             const updatedData = req.body;
-            const filter = {_id: ObjectId(id)};
+            const filter = { _id: ObjectId(id) };
             const options = { upsert: false };
             const updatedDoc = {
                 $set: {
@@ -70,10 +70,10 @@ const run = async() =>{
 
         //Restock Functionality
 
-        app.put('/restock/:id', async(req, res) =>{
+        app.put('/restock/:id', async (req, res) => {
             const id = req.params.id;
             const updatedData = req.body;
-            const filter = {_id: ObjectId(id)};
+            const filter = { _id: ObjectId(id) };
             const options = { upsert: false };
             const updatedDoc = {
                 $set: {
@@ -86,16 +86,16 @@ const run = async() =>{
         });
 
         //add a furniture 
-        app.post('/manage/add', async(req, res) =>{
+        app.post('/manage/add', async (req, res) => {
             const addedProduct = req.body;
             const newProduct = {
-                "image" : addedProduct.productImageUrl,
-                "email" : addedProduct.email,
-                "name" : addedProduct.productName,
-                "description" : addedProduct.productDescription,
-                "price" : addedProduct.productPrice,
-                "supplier" : addedProduct.productSupplier,
-                "quantity" : addedProduct.productQuantity
+                "image": addedProduct.productImageUrl,
+                "email": addedProduct.email,
+                "name": addedProduct.productName,
+                "description": addedProduct.productDescription,
+                "price": addedProduct.productPrice,
+                "supplier": addedProduct.productSupplier,
+                "quantity": addedProduct.productQuantity
 
             };
             const result = await productCollection.insertOne(newProduct);
@@ -105,22 +105,35 @@ const run = async() =>{
 
         //delete a furniture
 
-        app.delete('/delete/:id', async(req, res) =>{
-                const id = req.params.id;
-                const query = {_id : ObjectId(id)};
-                const results = await productCollection.deleteOne(query);
-                res.send(results);
+        app.delete('/delete/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const results = await productCollection.deleteOne(query);
+            res.send(results);
+        })
+
+        //fetch my inventory
+
+        app.get('/myinventory', async (req, res) => {
+            const email = req.query.email;
+            //
+            const query = { email: email };
+            const cursor = orderCollection.find(query);
+            const orders = await cursor.toArray();
+            res.send(orders);
+
+
         })
 
 
-    
 
- 
+
+
 
 
 
     }
-    finally{
+    finally {
 
 
 
@@ -131,14 +144,14 @@ const run = async() =>{
 run().catch(console.dir);
 
 //home api 
-app.get('/', async(req, res) =>{
-    res.json({message : 'hello world'})
+app.get('/', async (req, res) => {
+    res.json({ message: 'hello world' })
 
 })
 
 
 
 //port
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log("listening to the port", port)
 })
